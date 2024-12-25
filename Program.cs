d2cc92c0087ab04118c409d54e61c0e38f3a1c62
SaihajMann <Saihajmann07@gmail.com>
2024-12-25 10:46:52 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World!");
+Random rnd = new Random();
+int number = (int)Math.Floor(Math.Sqrt(rnd.Next(1, 10000)));
+Console.WriteLine("Your lucky number is");
+Console.WriteLine(number);
+Console.ReadKey();
