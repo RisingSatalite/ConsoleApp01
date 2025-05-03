7cd083edd93e24a26fc3df9d0215b5dc14ab4cba
SaihajMann <saihajmann07@gmail.com>
2025-05-03 09:54:27 -0600

    more function

--- MathFunctions.cs
+++ MathFunctions.cs
@@ -29,5 +29,31 @@ namespace MathSimple
             Console.WriteLine("Your number is");
             Console.WriteLine(number / number2);
         }
+
+        public static void Add()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number to add:");
+            float number = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a 2nd number to add:");
+            float number2 = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your number is");
+            Console.WriteLine(number + number2);
+        }
+
+        public static void Subtract()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number to subtract:");
+            float number = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a 2nd number to subtract by:");
+            float number2 = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your number is");
+            Console.WriteLine(number - number2);
+        }
     }
 }
